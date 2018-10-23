68196fb3097904ec16faf2b70dbcec6543cf9462
andrewcr101 <andrew.crawford@ge.com>
2018-10-23 19:38:43 -0400

    heroku v2

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,14 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if ('mysql://wj4t91jrhtjzoq7h:slvubdjb8np6iwc9@qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gre63iphc26rhmsp') {
+  connection = mysql.createConnection('mysql://wj4t91jrhtjzoq7h:slvubdjb8np6iwc9@qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gre63iphc26rhmsp');
   } else {
     connection = mysql.createConnection({
       host: 'localhost',
+      port: 3306,
       user: 'root',
-      password: 'Batma94life',
+      password: 'root',
       database: 'burger_db'
     });
   };
