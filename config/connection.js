123d0fa90a3590b7333a50da5faf42c079c02a68
andrewcr101 <andrew.crawford@ge.com>
2018-10-23 18:29:04 -0400

    final version

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+//This is the connction to the burger dataase
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//exports connection to orm.js
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Andrew Double Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('ATL Special Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('What-a-Burger Double');
+INSERT INTO burgers (burger_name) VALUES ('Peanut Butter Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheesy Cheese Double Burger');
